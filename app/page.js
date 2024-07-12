81fc6bf7c1c4c91c73a666debc43e9f40caa1490
Locuzzed MB Pro <locuzzed@gmail.com>
2024-07-12 21:52:37 +0200

    Adds HQ Icon and Apple Icon

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen">
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
       <div className="overflow-hidden">
         <Dashboard />
       </div>
